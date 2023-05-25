75671ae0861b222d8ad86986672276b15d6b0080
GhostAndry <andrearonzoni20062022@gmail.com>
2023-05-25 14:56:22 +0200

    Primo Commit

---
+++ src/main/java/me/ghostdevelopment/supergilde/SuperGilde.java
@@ -0,0 +1,17 @@
+package me.ghostdevelopment.supergilde;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperGilde extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
